1015e55fe64384c265937bdfb8d3cc76496350b0
ETmbit <ETmbit@outlook.com>
2026-03-27 14:59:50 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -33,14 +33,12 @@ namespace SimonSays {
     let points = 0
 
     export function clearColor() {
-        serial.writeLine("clearColor")
         pins.digitalWritePin(DigitalPin.P14, 0)
         pins.digitalWritePin(DigitalPin.P15, 0)
         pins.digitalWritePin(DigitalPin.P16, 0)
     }
 
     export function clearSeries() {
-        serial.writeLine("clearSeries")
         busy = false
         series = []
         ixseries = 0
@@ -51,15 +49,12 @@ namespace SimonSays {
     }
 
     export function restartSeries() {
-        serial.writeLine("restartSeries")
         ixseries = 0
         curColor = Color.None
         butColor = Color.None
     }
 
     export function showCurrentColor() {
-        serial.writeLine("showCurrentColor")
-        serial.writeLine("# color=" + curColor.toString())
         clearColor()
         basic.pause(500)
         switch (curColor) {
@@ -82,8 +77,6 @@ namespace SimonSays {
     }
 
     export function waitForButton(): Color {
-        serial.writeLine("waitForButton")
-        serial.writeLine("# timeout=" + tmout.toString())
         butColor = Color.None
         let tm = control.millis() + tmout
         while (tm > control.millis()) {
@@ -104,12 +97,10 @@ namespace SimonSays {
                     }
             basic.pause(1)
         }
-        serial.writeLine("# color=" + butColor.toString())
         return butColor
     }
 
     export function extendSeries() {
-        serial.writeLine("extendSeries")
         let clr = General.randomInt(0, 2)
         let color: Color
         switch (clr) {
@@ -117,34 +108,27 @@ namespace SimonSays {
             case 1: color = Color.Yellow; break
             case 2: color = Color.Blue; break
         }
-        serial.writeLine("# color=" + color.toString())
         series.push(color)
     }
 
     export function getSeriesLength(): number {
-        serial.writeLine("getSeriesLength")
-        serial.writeLine("# length=" + series.length.toString())
         return series.length
     }
 
     export function setFirstColor() {
-        serial.writeLine("setFirstColor")
         ixseries = 0
         tmout = tminit
         if (series.length) {
             busy = true
             curColor = series[ixseries]
-            serial.writeLine("# color=" + curColor.toString())
         }
         else {
             busy = false
             curColor = Color.None
-            serial.writeLine("# [not playing]")
         }
     }
 
     export function setNextColor() {
-        serial.writeLine("setNextColor")
         tmout -= tmdelta
         if (tmout < 100) tmout = 100
         if (ixseries < series.length)
@@ -152,78 +136,54 @@ namespace SimonSays {
         if (ixseries == series.length) {
             busy = false
             curColor = Color.None
-            serial.writeLine("# [not playing]")
         }
         else {
             curColor = series[ixseries]
-            serial.writeLine("# color=" + curColor.toString())
         }
     }
 
     export function isSeriesEnd(): boolean {
-        serial.writeLine("isSeriesEnd")
-        serial.writeLine("# " + (ixseries >= series.length ? "true" : "false"))
         return (ixseries >= series.length)
     }
 
     export function isSeriesBusy(): boolean {
-        serial.writeLine("isSeriesBusy")
-        serial.writeLine("# " + (busy ? "true" : "false"))
         return (busy)
     }
 
     export function getCurrentIndex(): number {
-        serial.writeLine("getCurrentIndex")
-        serial.writeLine("# index=" + ixseries.toString())
         return (ixseries >= 0 && ixseries < series.length ? ixseries : -1)
     }
 
     export function getCurrentColor(): Color {
-        serial.writeLine("getCurrentColor")
-        serial.writeLine("# color=" + curColor.toString())
         return curColor
     }
 
     export function getButtonColor(): Color {
-        serial.writeLine("getSelectedColor")
-        serial.writeLine("# color=" + butColor.toString())
         return butColor
     }
 
     export function isMatchingColor(): boolean {
-        serial.writeLine("isMatchingColor")
-        serial.writeLine("# curColor=" + curColor.toString() + ", butColor=" + butColor.toString())
         return (curColor == butColor)
     }
 
     export function increasePoints() {
-        serial.writeLine("increasePoints")
         points += 1
-        serial.writeLine("# points=" + points.toString())
     }
 
     export function getPoints(): number {
-        serial.writeLine("getPoints")
-        serial.writeLine("# points=" + points.toString())
         return points
     }
 
     export function setInitTimeout(timeout: number) {
-        serial.writeLine("setInitTimeout")
         tminit = timeout * 1000
-        serial.writeLine("# tminit=" + tminit.toString())
     }
 
     export function setFirstTimeout(timeout: number) {
-        serial.writeLine("setFirstTimeout")
         tmout = tminit
-        serial.writeLine("# tmout=" + tmout.toString())
     }
 
     export function setNextTimeout() {
-        serial.writeLine("setNextTimeout")
         if (tmout >= tmdelta) tmout -= tmdelta
-        serial.writeLine("# tmout=" + tmout.toString())
     }
 }
 
